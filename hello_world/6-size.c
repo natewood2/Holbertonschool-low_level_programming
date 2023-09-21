8c48a5b4cc74b4c4e4ce37d408b8f501c9bdc479
natewood2 <nathanwood.sfg@gmail.com>
2023-09-21 14:00:06 -0700

    -m64 for -world

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * main - Entry point
+ *
  * Return: Always 0
  */
+
 int main(void)
 {
-char acharacter;
-int ainteger;
-long along;
-long long alonglong;
-float afloat;
-
-printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of short: %zu bytes\n", sizeof(short));
-printf("Size of long: %zu bytes\n", sizeof(long));
-printf("Size of long long: %zu bytes\n", sizeof(long long));
-printf("Size of float: %zu bytes\n", sizeof(float));
-return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
 }
