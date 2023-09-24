84b7feaa9a1c745d004555bdca201615dc3f70ac
natewood2 <nathanwood.sfg@gmail.com>
2023-09-24 11:06:58 -0700

    based

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints number
+ * Description: "prints all the numbers of base 16 in lowercase."
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
