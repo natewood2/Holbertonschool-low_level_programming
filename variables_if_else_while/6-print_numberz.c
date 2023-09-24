241bed5b7ab29709aee711550451d0787cee91a5
natewood2 <nathanwood.sfg@gmail.com>
2023-09-24 10:29:08 -0700

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers
+ * Description: "prints all single digit numbers of base 10 starting from 0"
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
