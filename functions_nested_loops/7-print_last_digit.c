f716e9af75864182e1974fa041c1f3d1c5090f9a
natewood2 <nathanwood.sfg@gmail.com>
2023-09-25 11:34:40 -0700

    yippie'

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,18 +6,17 @@
  */
 int print_last_digit(int n)
 {
-	int a;	
+	int a;
 
 	if (n < 0)
-		n = -n;
+	n = -n;
 
 	a = n % 10;
-		
+
 	if (a < 0)
-		a = -a;
-	
+	a = -a;
+
 	_putchar(a + '0');
-	
+
 	return (a);
-		
 }
