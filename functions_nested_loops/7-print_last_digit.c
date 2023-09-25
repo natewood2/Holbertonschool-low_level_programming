c768eebeb173f299787433e18f774c9a8600a146
natewood2 <nathanwood.sfg@gmail.com>
2023-09-25 11:23:10 -0700

    added wrong number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,23 @@
 #include "main.h"
 /**
- * print_last_digit - a function that prints the last digit of a number.
- * @nld: number's last digit result
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
  * Return: value of the last digit
  */
-
-int print_last_digit(int nld)
-
+int print_last_digit(int n)
 {
-	int pld;
+	int a;	
 
-	pld = (nld % 10);
+	if (n < 0)
+		n = -n;
 
-	if (pld < 0)
-	{
-		pld = (-1 * pld);
-	}
-	_putchar(pld + '0');
-	return (0);
+	a = n % 10;
+		
+	if (a < 0)
+		a = -a;
+	
+	_putchar(a + '0');
+	
+	return (a);
+		
 }
