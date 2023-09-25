8b86a07c5c572b8cf9648f0bef7e1f8bc539737e
natewood2 <nathanwood.sfg@gmail.com>
2023-09-24 20:09:46 -0700

    coding

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line.
+ * Description: "prints _putchar, followed by a new line."
+ * Return: 0
+ */
+int main(void)
+{
+	char a[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
