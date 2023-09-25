0e3f9b456a571c82621fcf31760cd7fa2064c89a
natewood2 <nathanwood.sfg@gmail.com>
2023-09-25 13:25:54 -0700

    literally so confusing

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result.
+ * Description: adds two integers and returns a result.
+ * @o: first number input
+ * @s: second number input
+ * Return: one + two
+ */
+
+int add(int o, int s)
+
+{
+	return (o + s);
+}
