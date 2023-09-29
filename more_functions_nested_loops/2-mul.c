05226018d3844044e9405abaacb01a1e44ea0b19
natewood2 <nathanwood.sfg@gmail.com>
2023-09-29 05:57:46 -0700

    math island

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: the first integer
+ * @b: secind integer
+ * Return: the sum of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
