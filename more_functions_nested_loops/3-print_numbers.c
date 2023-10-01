ed4e27aadb8041c178512d03733481311f329b68
natewood2 <nathanwood.sfg@gmail.com>
2023-10-01 04:12:27 -0700

    _putchar

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - entry point
+ * Description: prints the number.
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
