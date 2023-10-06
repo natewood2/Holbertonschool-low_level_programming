424c671d43f6c45cf989d750af98193141e696f7
natewood2 <nathanwood.sfg@gmail.com>
2023-10-06 13:27:59 -0700

    rev array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - a function that reverses the content of an array of integers
+ * @a: input value
+ * @n: input value
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n--; i++)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
