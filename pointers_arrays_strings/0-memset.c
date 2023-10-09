f30164c83fec3c96a759198c4728173d6320b732
natewood2 <nathanwood.sfg@gmail.com>
2023-10-09 11:01:27 -0700

    function that fills memory

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - a function that fills memory with a constant byte.
+ * @s: target
+ * @b: constant byte
+ * @n: number of byte
+ * Return: new value of target.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
