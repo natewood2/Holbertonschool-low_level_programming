06b2be0dae35f750e2e3f0e4cc92d0d3f2a26f3d
natewood2 <nathanwood.sfg@gmail.com>
2023-10-16 12:36:48 -0700

    Straight Up

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: agrument count
+ * @argv: agrument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
