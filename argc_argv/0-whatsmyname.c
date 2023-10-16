9929a842a15ffeb7daf7843af9074fa3099d8d5e
natewood2 <nathanwood.sfg@gmail.com>
2023-10-16 11:45:12 -0700

    my name is

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+In C, argc and argv are parameters commonly used in the main function when writing command-line programs. They are used to handle command-line arguments passed to your C program when it is executed.
