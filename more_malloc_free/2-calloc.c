c867e8dffdb19170f37454a4a864941dd6d4be0f
natewood2 <nathanwood.sfg@gmail.com>
2023-10-23 13:33:51 -0700

    hot fix

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,18 +9,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 
 {
-	char *block;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	block = malloc(nmemb * size);
-	if (block != NULL)
-	{
-		for (i = 0; i < (nmemb * size); i++)
-			block[i] = 0;
-		return (block);
-	}
-	else
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+	p[i] = 0;
+	return (p);
 }
