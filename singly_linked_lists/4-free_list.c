47853a4ca08e825a5cee0bac7d6656fecf6f3cf6
natewood2 <nathanwood.sfg@gmail.com>
2023-11-09 11:21:20 -0800

    No memory leaks possible

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - free some nodes from a linked list
+ * @head: head to the node
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		list_t *temp_node = head;
+		head = head->next;
+
+		free(temp_node->str);
+		free(temp_node);
+	}
+}
