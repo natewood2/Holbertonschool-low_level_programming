a327db921f38281e1a732ed21b2c346c18256421
natewood2 <nathanwood.sfg@gmail.com>
2023-11-11 14:56:44 -0800

    freed again

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deleting and freeing nodes
+ * @head: head of the node
+ * Return: data pointer
+ */
+
+int pop_listint(listint_t **head)
+{
+	int data;
+
+	listint_t *temp_node;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (0);
+	}
+
+	temp_node = *head;
+	data=temp_node->n;
+
+	*head = (*head)->next;
+	free(temp_node);
+
+	return (data);
+}
