199aa7a5129bbcbfd79741162a1ca99677784904
natewood2 <nathanwood.sfg@gmail.com>
2023-11-11 13:42:00 -0800

    Added the nodes at the end this time

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds the node at the end
+ * @head: head of the node.
+ * @n: data that will be stored in the new node!
+ * Return: new_ndoe
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		listint_t *current_node = *head;
+
+		while (current_node->next != NULL)
+		{
+			current_node = current_node->next;
+		}
+		current_node->next = new_node;
+	}
+	return (new_node);
+}
