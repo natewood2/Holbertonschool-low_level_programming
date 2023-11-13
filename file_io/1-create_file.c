7a26facd1244ab1c396d4b0aff942a57fc5e1d0e
natewood2 <nathanwood.sfg@gmail.com>
2023-11-13 15:55:02 -0800

    I hope this also works

---
+++ file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file and gives it permissions
+ * @filename: the name of the file
+ * @text_content: the content of the text in the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		size_t len = 0;
+
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+		if (write(fd, text_content, len) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
+
