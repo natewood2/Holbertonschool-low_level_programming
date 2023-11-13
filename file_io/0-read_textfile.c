6fa2bcafa49a8fb6b871f7e39f87563580091c09
natewood2 <nathanwood.sfg@gmail.com>
2023-11-13 15:29:57 -0800

    Compiled!

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,8 +11,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read = 0, bytes_written;
 	char *buffer;
 
-	buffer = malloc(sizeof(*buffer) * letters);
 	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(*buffer) * letters);
+	if (buffer == NULL)
 	{
 		return (0);
 	}
@@ -23,11 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
-	if (buffer == NULL)
-	{
-		return (0);
-	}
-	bytes_written = read(fd, buffer, letters);
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read <= 0)
 	{
 		close(fd);
@@ -36,12 +37,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written != bytes_read)
+	if (bytes_written < bytes_read)
 	{
 		close(fd);
 		free(buffer);
 		return (0);
 	}
+	close(fd);
 	free(buffer);
 	return (bytes_written);
 }
