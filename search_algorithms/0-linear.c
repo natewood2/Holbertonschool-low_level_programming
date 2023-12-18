e99c834ed6489dc514495c288b254735108f2b53
natewood2 <nathanwood.sfg@gmail.com>
2023-12-18 05:53:52 -0800

    adding everything I have worked on the last three days

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - a function that searches for a value in an array of integers
+ * @array: hold the value in the array that we are searching for.
+ * @size: size of the array
+ * @value: the value we are searching for
+ * Return: FIND
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	int find = -1;
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Checking value [%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				find = i;
+				break;
+			}
+		}
+	}
+	return (find);
+}
---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+/**
+ * binary_search - a searching algorithem that sorts using the binary
+ * @array: the array we are sorting
+ * @size: size of the array
+ * @value: the value we are getting
+ * Return: -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int mid;
+	int high = size - 1;
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (low <= high)
+	{
+		printf("Current search range: ");
+		for (i = low; i <= high; i++)
+		{
+			if (i > low)
+				printf(", ");
+			printf("%d", array[i]);
+		}
+		printf("\n");
+
+		mid = low + (high - low) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
+}
